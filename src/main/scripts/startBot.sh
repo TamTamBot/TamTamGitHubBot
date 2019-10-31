be5a6d0a222c3ec65d588be1f6978ec77176d1a2
senyast4745 <senyasdt4745@gmail.com>
2019-10-31 03:00:01 +0300

    Add start/stop/restart script

--- src/main/scripts/startBot.sh
+++ src/main/scripts/startBot.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
-cd ..
-java -Dspring.config.location=file:config/ -jar testbot-0.3.jar
+command=$1
+filename=pid.txt
+scriptFolred=./scripts/
+
+function start {
+  cd ..
+  java -Dspring.config.location=file:config/ -jar testbot-0.3.jar &
+  PID=$!
+  echo $PID > "$scriptFolred$filename"
+}
+
+function stop {
+  pid=$(head -n 1 $filename)
+  kill "$pid"
+}
+
+function mainFunc {
+  echo "Your command: ${command}"
+  case $command in
+  start)
+    start
+    ;;
+    stop)
+      stop
+      rm $filename
+      ;;
+  restart)
+    if test -f "$filename"; then
+    stop
+    fi
+    start
+    ;;
+  *)
+    echo "Sorry, command ${command} not found."
+  esac
+}
+
+
+
+mainFunc
